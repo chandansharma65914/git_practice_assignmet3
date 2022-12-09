52feaef37af78011cacc85bd55f82d9645b2474c
chandan sharma <chandansharma695914@gmail.com>
2022-12-09 23:20:44 +0530

    please correct my code

---
+++ prime.js
@@ -0,0 +1,11 @@
+   let count=0;
+   for(let i=1;i<n;i++){
+    if(n%i==0){
+        count++;
+    }
+   }
+   if(count==10){                            //put 2 at the place of 10
+    console.log("Prime")
+   }else{
+    console.log("Not Prime")
+   }
